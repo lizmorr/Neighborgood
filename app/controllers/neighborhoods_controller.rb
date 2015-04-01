3ea6153cb67b8d125457d4c87ffe41b015b568d5
ectomorph314 <tns-GitHub@aranfell.com>
2015-04-01 10:37:05 -0400

    Add edit feature

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -21,6 +21,20 @@ class NeighborhoodsController < ApplicationController
     end
   end
 
+  def edit
+    @neighborhood = Neighborhood.find(params[:id])
+  end
+
+  def update
+    @neighborhood = Neighborhood.find(params[:id])
+    if @neighborhood.update_attributes(neighborhood_params)
+      redirect_to neighborhood_path(@neighborhood), notice: "Neighborhood Edited!"
+    else
+      flash[:alert] = "Your neighborhood was not saved"
+      render :edit
+    end
+  end
+
   protected
 
   def neighborhood_params
---
+++ spec/features/user_edits_neighborhood_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+feature "user edits neighborhood", %(
+  As the original poster
+  I want to be able to update a neighborhood's information
+  So I can correct errors or provide new information.
+) do
+
+  scenario "user successfully edits neighborhood" do
+    neighborhood = FactoryGirl.create(:neighborhood)
+
+    visit edit_neighborhood_path(neighborhood)
+    fill_in "Name", with: "Uptown"
+    select("West", from: "Location")
+    fill_in "Description", with: "Super!"
+    click_on "Submit"
+
+    expect(page).to have_content("Uptown")
+    expect(page).to have_content("West")
+    expect(page).to have_content("Super!")
+  end
+
+  scenario "user unsuccessfully edits neighborhood" do
+    neighborhood = FactoryGirl.create(:neighborhood)
+
+    visit edit_neighborhood_path(neighborhood)
+    fill_in "Name", with: "Hi"
+    fill_in "Description", with: "!"
+    click_on "Submit"
+
+    expect(page).to have_content("Your neighborhood was not saved")
+    expect(page).to have_content("Name is too short (minimum is 4 characters)")
+    expect(page).to have_content("Description is too short (minimum is 2 characters)")
+  end
+end
