cecd9b86478fd1dba5c6afc995cfc7893c5dd091
Dodie324 <drmoore324@gmail.com>
2015-04-01 11:38:22 -0400

    Update index so that reviews are sorted by time; add test to check

--- spec/features/user_views_reviews_spec.rb
+++ spec/features/user_views_reviews_spec.rb
@@ -6,6 +6,17 @@ feature "user views all reviews for neighborhood", %(
   So that I could learn more about that neighborhood.
 ) do
 
+  scenario "reviews are sorted by create time" do
+    neighborhood = FactoryGirl.create(:neighborhood)
+    old_review = FactoryGirl.create(:review, neighborhood: neighborhood, created_at: 5.days.ago)
+    new_review = FactoryGirl.create(:review, description: "So cool", neighborhood: neighborhood)
+
+    visit neighborhood_path(neighborhood)
+
+
+    expect(page.body.index(old_review.description) > page.body.index(new_review.description))
+  end
+
   scenario "viewer views reviews of neighborhood" do
     neighborhood = FactoryGirl.create(:neighborhood)
     review = FactoryGirl.create(:review, neighborhood: neighborhood)
