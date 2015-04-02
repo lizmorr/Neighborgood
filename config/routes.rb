b5fa1f02f266159a6b96e16f8272c2d072d62d56
kyjoya <kyleejacker@gmail.com>
2015-04-02 15:28:24 -0400

    Update routes for votes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :neighborhoods, except: :destroy do
-    resources :reviews, except: :show
+    resources :reviews, except: :show do
+      scope module: :votes do
+        resources :upvote
+        resources :downvote
+      end
+    end
   end
 end
