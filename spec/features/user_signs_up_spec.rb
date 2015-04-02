c1605b47565599bfa4918a4337131468b8c27cc3
kyjoya <kyleejacker@gmail.com>
2015-04-02 16:13:01 -0400

    tested for duplicate user

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -32,4 +32,25 @@ feature 'user registers', %Q{
     expect(page).to have_content("can't be blank")
     expect(page).to_not have_content('Sign Out')
   end
+
+  scenario 'email is not unique' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+    click_on 'Sign Out'
+
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+    
+    expect(page).to have_content("Email has already been taken")
+  end
 end
