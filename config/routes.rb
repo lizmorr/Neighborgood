c57e19be799cf1aa930ad752d0b76398b168a495
Daniel Moore <drmoore324@gmail.com>
2015-04-02 14:37:12 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
     :create, :edit, :update] do
     resources :reviews, only: [:index, :new, :create, :edit, :update, :destroy]
   end
-
-  # resources :reviews, only: [:show, :edit]
 end
