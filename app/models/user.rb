eb581a4a91bd9b5666cf64e93c72ad8c7478cc8b
ectomorph314 <tns-GitHub@aranfell.com>
2015-04-03 14:41:55 -0400

    Fix Hound issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
   has_many :neighborhoods
   has_many :reviews
 
-  def self.current
-    Thread.current[:user]
-  end
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
