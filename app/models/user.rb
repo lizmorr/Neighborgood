e37d0d5e6a01fd69354bc60b1f8956cd9693fbdb
ectomorph314 <tns-GitHub@aranfell.com>
2015-04-06 10:53:56 -0400

    Add admin column

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :neighborhoods
   has_many :reviews
 
+  ROLES = %w(member admin)
+
+  validates :role, presence: true, inclusion: { in: ROLES }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
