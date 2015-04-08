5b6ff458706e696d0a66c5818ae6f5935ff0b3f0
Dodie324 <drmoore324@gmail.com>
2015-04-08 13:16:02 -0400

    Add delete confirmation

---
+++ app/assets/javascripts/delete_confirmation.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#delete').on('click', function(e) {
+    e.preventDefault();
+    var confirm = window.confirm("Are you sure you want to delete?")
+    if( confirm == true ){
+        return true;
+    } else {
+        return false;
+    }
+  });
+});
