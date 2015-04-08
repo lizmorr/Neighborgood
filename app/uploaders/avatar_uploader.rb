d376fe47c864f7151de87419dca61f76cc0d447c
ectomorph314 <tns-GitHub@aranfell.com>
2015-04-08 10:49:01 -0400

    Add carrierwave test

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,7 +6,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
 
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
--- spec/features/neighborhoods/admin_creates_new_neighborhood_spec.rb
+++ spec/features/neighborhoods/admin_creates_new_neighborhood_spec.rb
@@ -15,11 +15,14 @@ feature "admin can create new neighborhood", %(
     fill_in "Name", with: "Downtown"
     select("East", from: "Location")
     fill_in "Description", with: "Lame."
+    attach_file("neighborhood_image", "spec/fixtures/Stonehenge.jpg")
     click_on "Submit"
 
+    neighborhood = Neighborhood.last
     expect(page).to have_content("Neighborhood Added!")
-    expect(page).to have_content("Downtown")
-    expect(page).to have_content("EAST")
+    expect(page).to have_content(neighborhood.name)
+    expect(page).to have_content(neighborhood.location.upcase)
+    expect(page).to have_css("img[src*='#{neighborhood.image.url}']")
   end
 
   scenario "admin attempts to add invalid neighborhood" do
@@ -45,5 +48,16 @@ feature "admin can create new neighborhood", %(
   scenario "visitor attempts to add neighborhood" do
     visit new_admin_neighborhood_path
     expect(page).to have_content("You don't have access to this page!")
+  end
+
+  scenario "admin attempts to add wrong image type" do
+    admin = FactoryGirl.create(:admin_user)
+    sign_in_as(admin)
+    visit new_admin_neighborhood_path
+
+    attach_file("neighborhood_image", "spec/fixtures/Stonehenge.pdf")
+    click_on "Submit"
+
+    expect(page).to have_content(`Image You are not allowed to upload "pdf" files, allowed types: jpg, jpeg, gif, png`)
   end
 end
