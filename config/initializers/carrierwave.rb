859e99397507035ca62e400d23fff260be79129a
Spencer Dixon <spencercdixon@gmail.com>
2015-04-09 10:11:47 -0400

    Fixed travis ci bug with aws keys, set up basic sass system

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.development?
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'] || '',
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] || ''
     }
 
-    config.fog_directory = ENV['AWS_BUCKET']
+    config.fog_directory = ENV['AWS_BUCKET'] || ''
 
     config.storage :fog
   else
