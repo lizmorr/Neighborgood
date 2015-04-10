0ab8d5ee74e856bd96cef8245ac04028be44e8cf
kyjoya <kyleejacker@gmail.com>
2015-04-10 15:18:35 -0400

    changed password to apikey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
   :port      => '587',
-  # :enable_starttls_auto => true, # detects and uses STARTTLS
+  :enable_starttls_auto => true, # detects and uses STARTTLS
   :user_name => ENV["MANDRILL_USERNAME"],
-  :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+  :password  => ENV["MANDRILL_APIKEY"], # SMTP password is any valid API key
   :authentication => :plain, # Mandrill supports 'plain' or 'login'
   :domain => 'heroku.com' # your domain to identify your server when connecting
 }
