85c67b33e7f38f43393f63229c47292c46f0d23e
kyjoya <kyleejacker@gmail.com>
2015-04-10 14:59:17 -0400

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,12 +2,12 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
-  :port      => 587
+  :port      => 587,
   :enable_starttls_auto => true, # detects and uses STARTTLS
   :user_name => ENV["MANDRILL_USERNAME"],
   :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
   :authentication => 'login', # Mandrill supports 'plain' or 'login'
-  :domain => 'heroku.com', # your domain to identify your server when connecting
+  :domain => 'heroku.com' # your domain to identify your server when connecting
 }
 
   # Code is not reloaded between requests.
