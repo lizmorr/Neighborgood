5b6069d69d616dba072f118f4e5883b7dbd57454
kyjoya <kyleejacker@gmail.com>
2015-04-10 15:03:44 -0400

    made line 5 on production.rb a string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.smtp_settings = {
   :address   => "smtp.mandrillapp.com",
-  :port      => 587,
+  :port      => '587',
   :enable_starttls_auto => true, # detects and uses STARTTLS
   :user_name => ENV["MANDRILL_USERNAME"],
   :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
